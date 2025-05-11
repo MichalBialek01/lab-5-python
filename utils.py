b7458917a5dbaebf6e43a5f5d573280861dd0225
Michał Białek <kontakt.michalbialek@gmail.com>
2025-05-11 16:29:49 +0200

    init: add comments

--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
-# Exemplary calculator functions
+"""Simple calculator functions for CI/CD testing."""
 
 def add(a: int, b: int) -> int:
+    """Return the sum of two integers."""
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """Return the difference between two integers."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Return the product of two integers."""
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """Return the division result of two integers as a float."""
     return a / b
